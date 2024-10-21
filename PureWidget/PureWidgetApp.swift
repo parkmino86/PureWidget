30d4f9298cd969d4cb4baafeaddbef4455ce3c16
minoh.park <parkmino86@gmail.com>
2024-10-21 23:06:37 +0900

    Initial Commit

---
+++ PureWidget/PureWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PureWidgetApp.swift
+//  PureWidget
+//
+//  Created by minoh.park on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct PureWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
